0302a7ca5d92be022a0adebf17bff3f356b31f37
unknown <mishen@mjlshen.(none)>
2014-01-19 21:51:41 -0500

    PORT fix

--- time2eat.py
+++ time2eat.py
@@ -19,4 +19,5 @@ def unscrambleWord():
 		return render_template('index.html', anagrams = inputAnagrams.replace(',', ', ') + "<hr>" + request.form['previousAnagrams'], static = False)
 
 if __name__ == "__main__":
-	app.run()
+        port = int(os.environ.get("PORT", 5000)
+	app.run(host='0.0.0.0', port=port)
