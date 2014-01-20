540c3cf2f5a8698ea9ec563598f8af2256a015a0
unknown <mishen@mjlshen.(none)>
2014-01-19 21:28:47 -0500

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python dealscraper/time2eat.py
---
+++ time2eat.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, render_template
+import os, csv
+
+app = Flask(__name__)
+
+recipes = {key: value for key, value in csv.reader(open("recipes"))}
+
+@app.route('/', methods=['GET', 'POST'])
+def unscrambleWord():
+	if request.method == 'GET':
+		return render_template('index.html')
+	if request.method == 'POST':
+		try:
+			inputAnagrams = recipes[(''.join([i for i in request.form['inputAnagrams']]))]
+		except KeyError:
+			return render_template('index.html', anagrams = request.form['previousAnagrams'], static = True)
+		if request.form['previousAnagrams'] == '':
+			return render_template('index.html', anagrams = inputAnagrams.replace(',', ', '), static = False)
+		return render_template('index.html', anagrams = inputAnagrams.replace(',', ', ') + "<hr>" + request.form['previousAnagrams'], static = False)
+
+if __name__ == "__main__":
+	app.run()
