1bbf1035b2f4ad341d425284737c0ff7269c6e94
Michael Shen <mjlshen@yahoo.com>
2014-01-22 11:23:43 -0500

    Updated for home page

--- time2eat.py
+++ time2eat.py
@@ -3,25 +3,55 @@ import os, csv
 
 app = Flask(__name__)
 
-recipes = {key: value for key, value in csv.reader(open("recipes"))}
+recipes_break = {key: value for key, value in csv.reader(open("recipes_break"))}
+recipes_lunch = {key: value for key, value in csv.reader(open("recipes_lunch"))}
+recipes_dinner = {key: value for key, value in csv.reader(open("recipes_dinner"))}
 
-@app.route('/favicon.ico')
-def favicon():
-	return send_from_directory(os.path.join(app.root_path, 'static', 'images'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/', methods=['GET', 'POST'])
-def findRecipe():
+#@app.route('/favicon.ico')
+#def favicon():
+#	return send_from_directory(os.path.join(app.root_path, 'static', 'images'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/')
+def placeholder():
+	return render_template('index.html')
+@app.route('/breakfast', methods=['GET', 'POST'])
+def findRecipeb():
 	if request.method == 'GET':
-		return render_template('index.html')
+		return render_template('breakfast.html')
 	if request.method == 'POST':
 		try:
-			inputTime = recipes[(''.join([i for i in request.form['inputTime']]))]
+			inputTime = recipes_break[(''.join([i for i in request.form['inputTime']]))]
 		except KeyError:
-			return render_template('index.html', recipes = request.form['previousRecipes'], static = True)
+			return render_template('breakfast.html', recipes_break = request.form['previousRecipes'], static = True)
 		if request.form['previousRecipes'] == '':
-			return render_template('index.html', recipes = inputTime.replace(',', ', '), static = False)
-		return render_template('index.html', recipes = inputTime.replace(',', ', ') + "<hr>" + request.form['previousRecipes'], static = False)
+			return render_template('breakfast.html', recipes_break = inputTime.replace(',', ', '), static = False)
+		return render_template('breakfast.html', recipes_break = inputTime.replace(',', ', ') + "<hr>" + request.form['previousRecipes'], static = False)
+@app.route('/lunch', methods=['GET', 'POST'])
+def findRecipel():
+	if request.method == 'GET':
+		return render_template('lunch.html')
+	if request.method == 'POST':
+		try:
+			inputTime = recipes_lunch[(''.join([i for i in request.form['inputTime']]))]
+		except KeyError:
+			return render_template('lunch.html', recipes_lunch = request.form['previousRecipes'], static = True)
+		if request.form['previousRecipes'] == '':
+			return render_template('lunch.html', recipes_lunch = inputTime.replace(',', ', '), static = False)
+		return render_template('lunch.html', recipes_lunch = inputTime.replace(',', ', ') + "<hr>" + request.form['previousRecipes'], static = False)
+@app.route('/dinner', methods=['GET', 'POST'])
+def findReciped():
+	if request.method == 'GET':
+		return render_template('dinner.html')
+	if request.method == 'POST':
+		try:
+			inputTime = recipes_dinner[(''.join([i for i in request.form['inputTime']]))]
+		except KeyError:
+			return render_template('dinner.html', recipes_dinner = request.form['previousRecipes'], static = True)
+		if request.form['previousRecipes'] == '':
+			return render_template('dinner.html', recipes_dinner = inputTime.replace(',', ', '), static = False)
+		return render_template('dinner.html', recipes_dinner = inputTime.replace(',', ', ') + "<hr>" + request.form['previousRecipes'], static = False)
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
-	app.run(host='0.0.0.0', port=port)
+	app.run(host='0.0.0.0', port=port, debug = True)
